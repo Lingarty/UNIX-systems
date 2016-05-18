097731ba3a27dbfde9be4d5343be00ed0728a73a
Lingarty <lingarty@gmail.com>
2016-05-18 22:02:40 +0400

    Create script1.bash

---
+++ script1.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Генерирует папку с файлами для теста второго скрипта
+
+mkdir 4-test
+cd 4-test
+touch file1 file2 file3 file4 file5 file6 file7 file8 file9 file10 file11 file12 file13 file14 file15 file16 file17 file18 file19 file20
+ls -al
